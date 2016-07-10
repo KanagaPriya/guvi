106fb7a9b69cc3cf4a99d8512ebbefcd1f69ef38
KanagaPriya <kanagapriyakpr@gmail.com>
2016-07-10 12:51:24 +0530

    Update sapro12.c

--- sapro12.c
+++ sapro12.c
@@ -1,30 +1,16 @@
 #include<stdio.h>
-void main()
-{
-char name={"TREE"};
-int thickness;
-int branches;
-char color[];
-int angle;
-printf("enter the thickness of the tree");
-scanf("%d,&thickness of the tree");
-printf("enter the branches of the tree");
-scanf("%d,branches of the tree");
-printf("enter the color of the tree");
-scanf("%c,&color of the tree");
-printf("enter the angle of the tree");
-scanf("%d,&angle of the tree");
-if(thickness>23)
-{
-if (branches>20)
-{
-if(angle<=45)
-{
-printf("\n the angle of the tree is:");
+#include<conio.h>
+void add(node* *hd, int v){
+node* curr = *hd;
+if(curr == NULL){
+    curr = (node*)malloc(sizeof(node));
+    curr->value = v;
+}else{
+    if(v < curr->value){
+        add(&curr->left, v);
+    }else{
+        add(&curr->right, v);
+    }
 }
-printf("\n the branches of the tree is:");
-}
-printf("\n the thickness of the tree is:");
-}
-getch()
+getch;
 }
